732680f225e140a8b851e892bca423098dd75f99
Christopher Añorve <canorve@gmail.com>
2022-08-12 13:12:37 -0600

    license identifier changed  On branch master  Changes to be committed: 	modified:   setup.cfg 	modified:   src/clusex/pysex.py

--- src/clusex/pysex.py
+++ src/clusex/pysex.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
 
+#    CluSex is a set of routines that aids Sextractor 
+#    to perform on images of cluster galaxies
+#    Copyright (C) 2022  Christopher Añorve 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import numpy as np
 import sys
 import os
@@ -38,10 +56,6 @@ from clusex.lib.radcor import RadMod
 
 
 from clusex.lib.satbox import SatBox 
-
-# This program creates a catalog of Sextractor with
-# a combination of two runs of Sextractor with
-# different configuration parameters
 
 
 def main():
