e887d2b118cdbe185c6a3ef5f1e7066b8651bc8f
Christopher Añorve <canorve@gmail.com>
2026-06-26 14:45:39 -0700

    joinclass removed bug when joining files

--- src/clusex/lib/joinfiles.py
+++ src/clusex/lib/joinfiles.py
@@ -30,24 +30,50 @@ def joinclass(archivo1, archivo2, salida):
 
     # make sure the first columns is the ID 
     df2.rename(columns={0: "ID"}, inplace=True)
+    df2.rename(columns={1: "RA"}, inplace=True)
+    df2.rename(columns={2: "DEC"}, inplace=True)
+    df2.rename(columns={3: "X_IMAGE"}, inplace=True)
+    df2.rename(columns={4: "Y_IMAGE"}, inplace=True)
+    df2.rename(columns={5: "MAG_BEST"}, inplace=True)
+    df2.rename(columns={6: "KRON_RADIUS"}, inplace=True)
+    df2.rename(columns={7: "FLUX_RADIUS"}, inplace=True)
+    df2.rename(columns={8: "ISOAREA_IMAGE"}, inplace=True)
+    df2.rename(columns={9: "A_IMAGE"}, inplace=True)
+    df2.rename(columns={10: "ELLIPTICITY"}, inplace=True)
+    df2.rename(columns={11: "THETA_IMAGE"}, inplace=True)
+    df2.rename(columns={12: "BACKGROUND"}, inplace=True)
+    df2.rename(columns={13: "CLASS_STAR"}, inplace=True)
+    df2.rename(columns={14: "FLAGS"}, inplace=True)
 
     # 3) join using ID. It keeps the rows of df1 
-    merged = pd.merge(df1, df2, on="ID", how="left", sort=False)
+    merged = pd.merge(df1, df2, on="ID", how="left", sort=False, indicator=True)
+
+    #removing spaces:
+    df1.columns = df1.columns.str.strip()
+    df2.columns = df2.columns.str.strip()
+    merged.columns = merged.columns.str.strip()
 
     # 4) sort columns: first path, class, ID and other columns 
     otras_cols = [c for c in merged.columns if c not in ["path", 'ID' ,'class']]
     merged = merged[["path", 'ID' ,'class'] + otras_cols]
 
+
+
+    faltantes = (merged["_merge"] == "left_only").sum()
+
+    if faltantes:
+        print(f"Advertencia: {faltantes} filas sin coincidencia en archivo2.")
+
     # 5) Save output as CSV file 
+    merged.drop(columns=["_merge"], inplace=True)
     sep = "," if salida.endswith((".csv", ".txt")) else ","
     merged.to_csv(salida, index=False, sep=sep)
 
     # brief verification messages 
     print(f"rows file1: {len(df1)}")
     print(f"row output file: {len(merged)}")
-    faltantes = merged[merged.filter(regex=r"^\d+$").columns].isna().all(axis=1).sum() if len(df2.columns) > 1 else 0
-    if faltantes:
-        print(f"Advertencia: {faltantes} filas sin coincidencia en archivo2 (valores NaN).")
+
+
 
 #if __name__ == "__main__":
 
